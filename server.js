72601efdea8bcde87b47db57a79bacf935791989
Abhinav <abhinavdixit700@gmail.com>
2018-01-28 23:08:33 +0530

    Changed hbs to .hbs

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const fs= require('fs');
 var port = process.env.PORT || 3000 ;
 var app = express();
 
-app.set('view engine','hbs');
+app.set('view engine','.hbs');
+// app.set('Views', path.join(__dirname, 'Views'));
 app.use(express.static(__dirname + '/Public'));
 
 app.use((req,res,next)=>{
